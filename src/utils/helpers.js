8b58645914172070c5f782fb77a17df6889f5cfa
TallCricket <shivankp900@gmail.com>
2026-03-22 10:57:37 +0530

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24,14 +24,17 @@ export function formatCount(n) {
   return String(n || 0)
 }
 
+import React from 'react'
+
 export function processText(text) {
   if (!text) return ''
   // Split on hashtags and mentions
   const mentionRegex = new RegExp('(#\\w+|@\\w+)')
   const parts = text.split(mentionRegex)
   return parts.map((part, i) => {
-    if (part.startsWith('#')) return <span key={i} style={{ color: '#065fd4', fontWeight: 600 }}>{part}</span>
-    if (part.startsWith('@')) return <span key={i} style={{ color: '#065fd4', fontWeight: 600 }}>{part}</span>
+    if (part.startsWith('#') || part.startsWith('@')) {
+      return React.createElement('span', { key: i, style: { color: '#065fd4', fontWeight: 600 } }, part)
+    }
     return part
   })
 }
